707af951eb5c14a6ed7dd3780b7b893b2a54ad4d
Papasqunato <155753905+Papasqunato@users.noreply.github.com>
2024-09-20 15:35:54 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 import streamlit as st
 from snowflake.snowpark.functions import col
