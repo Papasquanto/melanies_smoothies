a60e8dc2d03335b33900fe687e7ba736060c2904
Papasqunato <155753905+Papasqunato@users.noreply.github.com>
2024-09-20 15:42:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
-
-
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 streamlit.title('MY parents healthy diner')
@@ -31,7 +30,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    
+              fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        #st.text(fruityvice_response.json())
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
 
@@ -45,9 +46,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
-        import requests
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-        #st.text(fruityvice_response.json())
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+  
         
         st.success('Your Smoothie is ordered!', icon="✅")
